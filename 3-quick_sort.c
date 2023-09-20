c254407fa2af002e28e0e48f5ad1e4428c0c7785
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-09-20 19:47:56 +0300

    quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,21 +4,37 @@
  * @array: array to sort
  * @size: size of the array
  */
-void quick_sort(int *array, int lb, int ub)
+void quick_sort(int *array, size_t size)
 {
-	/* int lb = 0;*/
-	/*int ub = size - 1;*/
+	if (size == 0 || array == NULL)
+		return;
+	quick_sort_a(array, 0, size - 1, size);
+}
+/**
+ * quick_sort_a - quick sort algorithm in c
+ * @array: array to sort
+ * @lb: size of the array
+ * @ub: upper bound
+ */
+void quick_sort_a(int *array, int lb, int ub, size_t size)
+{
 	int loc;
 
 	if (lb < ub)
 	{
-		loc = partition(array, lb, ub);
-		quick_sort(array, lb, loc - 1);
-		quick_sort(array, loc + 1, ub);
+		loc = partition(array, lb, ub, size);
+		quick_sort_a(array, lb, loc - 1, size);
+		quick_sort_a(array, loc + 1, ub, size);
 	}
 }
-
-int partition(int *array, int lb, int ub)
+/**
+ * partition - partition function
+ * @array: array
+ * @lb: lb
+ * @ub: ub
+ * Return: int
+ */
+int partition(int *array, int lb, int ub, size_t size)
 {
 	int pivot = array[lb];
 	int start = lb;
@@ -40,6 +56,7 @@ int partition(int *array, int lb, int ub)
 			temp = array[start];
 			array[start] = array[end];
 			array[end] = temp;
+			print_array(array, size);
 		}
 	}
 	temp = array[lb];
